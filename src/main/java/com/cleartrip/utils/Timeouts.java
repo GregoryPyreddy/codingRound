590f0fdbca784b6ea69c637496eb8771326a69a2
Gregory <gregoryanand@gmail.com>
2019-11-08 04:49:53 +0530

    Created an interface with some variables to signify the wait

---
+++ src/main/java/com/cleartrip/utils/Timeouts.java
@@ -0,0 +1,10 @@
+package com.cleartrip.utils;
+
+public interface Timeouts {
+	
+	public static int shortWait=5;
+	public static int mediumWait = 10;
+	public static int longWaait = 20;
+	public static int veryLongWait=60;
+
+}
